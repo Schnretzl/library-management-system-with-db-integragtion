a304c87139c2959577430d21d59ac2b72cc64b1e
Schnretzl <wrhendley@gmail.com>
2024-10-01 12:57:26 -0500

    Refactor add_author, view_author_details, find_author_index to use database

--- author_menu.py
+++ author_menu.py
@@ -1,29 +1,51 @@
-from author import Author
-
-def add_author(authors):
+def add_author(conn):
     name = input("Enter the name of the author: ")
     bio = input("Enter the biography of the author: ")
-    authors.append(Author(name, bio))
-    print(f"Added {name} to the list of authors.\n")
+    if conn is not None:
+        try:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM authors WHERE name = %s", (name,))
+            result = cursor.fetchone()
+            if result:
+                print("Author already exists.")
+                return False
+            cursor.execute("INSERT INTO authors (name, bio) VALUES (%s, %s)", (name, bio))
+            print(f"Added {name} to the list of authors.\n")
+            conn.commit()
+        except Exception as e:
+            print(f"Error: {e}")
     
-def view_author_details(authors):
+def view_author_details(conn):
     author_name = input("Enter the name of the author: ")
-    author_index = find_author_index(authors, author_name)
+    author_index = find_author_index(conn, author_name)
     if author_index is None:
         print("Author not found.")
         return False
-    print(f"Name: {authors[author_index].name}")
-    print(f"Biography: {authors[author_index].biography}")
-    print()
+    if conn is not None:
+        try:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM authors WHERE id = %s", (author_index,))
+            author = cursor.fetchone()
+            print(f"Name: {author[1]}")
+            print(f"Biography: {author[2]}")
+            print()
+        except Exception as e:
+            print(f"Error: {e}")
     
-def display_authors(authors):
+def display_authors(conn):
     for author in authors:
         print(f"Name: {author.name}")
     print()
         
-def find_author_index(authors, author_name):
+def find_author_index(conn, author_name):
     #Return the index of the author, or None if the author is not found
-    for index, author in enumerate(authors):
-        if author.name == author_name:
-            return index
+    if conn is not None:
+        try:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM authors WHERE name = %s", (author_name,))
+            result = cursor.fetchone()
+            author_id = result[0] if result else None
+            return author_id
+        except Exception as e:
+            print(f"Error: {e}")
     return None
