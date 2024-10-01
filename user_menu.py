aea0a3f8f1f4cd1f7bf7bb4b6e2c73e843eec430
Schnretzl <wrhendley@gmail.com>
2024-10-01 11:04:07 -0500

    Fix find_user_index.  Improve display in view_user_details

--- user_menu.py
+++ user_menu.py
@@ -24,17 +24,19 @@ def view_user_details(conn):
             user = cursor.fetchone()
             print(f"Name: {user[1]}")
             print(f"ID: {user[0]}")
-            print("Borrowed Books:")
             cursor.execute("SELECT * FROM borrowed_books WHERE user_id = %s AND return_date IS NULL", (user_index,))
             borrowed_books = cursor.fetchall()
-            for book in borrowed_books:
-                cursor.execute("SELECT * FROM books WHERE id = %s", (book[2],))
-                book_details = cursor.fetchone()
-                print(f"{book_details[1]}")
+            if len(borrowed_books) == 0:
+                print("No borrowed books.")
+            else:
+                print("Borrowed Books:")
+                for book in borrowed_books:
+                    cursor.execute("SELECT * FROM books WHERE id = %s", (book[2],))
+                    book_details = cursor.fetchone()
+                    print(f"{book_details[1]}")
             print()
         except Exception as e:
             print(f"Error: {e}")
-    print()
 
 def display_users(users):
     for user in users:
@@ -42,13 +44,13 @@ def display_users(users):
         print(f"ID: {user.id}")
         print()
         
-def find_user_index(conn, user):
-    #Return the index of the user, or None if the user is not found
+def find_user_index(conn, user_id):
     if conn is not None:
         try:
             cursor = conn.cursor()
-            cursor.execute("SELECT * FROM users WHERE name = %s", (user,))
-            user_index = cursor.fetchone()[0] if cursor.fetchone() else None
+            cursor.execute("SELECT * FROM users WHERE id = %s", (user_id,))
+            result = cursor.fetchone()
+            user_index = result[0] if result else None
             return user_index
         except Exception as e:
             print(f"Error: {e}")    
